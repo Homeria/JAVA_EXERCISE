effe8b6a0d4f50ecce01e1a986b5db2d700edd42
admin <admin@10.137.138.181>
2018-01-04 18:10:36 +0900

    Chapter05

---
+++ src/icehs/science/chapter05/GradeIfElseTest.java
@@ -0,0 +1,25 @@
+package icehs.science.chapter05;
+
+public class GradeIfElseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int gildongScore = 95;
+		int gilsoonScore = 88;
+		
+		if (gildongScore >= 90) {
+			System.out.println("홍길동 : A학점 입니다.");
+		} else {
+			System.out.println("홍길동 : A학점을 받지 못했습니다.");
+		}
+		
+		if (gilsoonScore >= 90) {
+			System.out.println("홍길순 : A학점 입니다.");
+		} else {
+			System.out.println("홍길순 : A학점을 받지 못했습니다.");
+		}
+		
+	}
+
+}
