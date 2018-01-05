1b9ee8ee470224ae70425b732fe25b9a61b500eb
admin <admin@10.137.138.181>
2018-01-05 17:05:51 +0900

    Chapter05

--- src/icehs/science/chapter05/PrimeNumberTest.java
+++ src/icehs/science/chapter05/PrimeNumberTest.java
@@ -11,9 +11,9 @@ public class PrimeNumberTest {
 		}
 		for(int i = 2; i < number; i++) {
 			if((number % i) == 0) {
-				System.out.println(number + "은 소수가 아닙니다."); 
+				System.out.println(number + "은 소수가 아닙니다."); break;
 			}else {
-				System.out.println(number + "은 소수입니다."); 
+				System.out.println(number + "은 소수입니다."); break;
 			}
 		}
 		
