5911dbfa8306b328c040b6c62dfd3c0eaffc8cc4
admin <admin@10.137.138.181>
2018-01-09 18:34:37 +0900

    Chapter07

---
+++ src/icehs/science/chapter07/StudentTest.java
@@ -0,0 +1,16 @@
+package icehs.science.chapter07;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		int input = getUserInput();
+		System.out.println(input);
+	}
+
+	public static int getUserInput() {
+		
+		return 0;
+	}
+	
+	
+}
