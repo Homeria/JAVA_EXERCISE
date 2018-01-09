be7c328576767e306faf47a0446d0476772d3339
admin <admin@10.137.138.181>
2018-01-09 16:49:02 +0900

    Chapter07

---
+++ src/icehs/science/chapter07/Television.java
@@ -0,0 +1,29 @@
+package icehs.science.chapter07;
+
+public class Television {
+
+	String name;
+	int price;
+	String description;
+	
+	public Television(String name, int price, String description) {
+		this.name = name;
+		this.price = price;
+		this.description = description;
+	}
+
+	
+	public Television(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	void printTelevisionInfo() {
+		if (description == null) {
+			System.out.println(name + " : " + price);
+		} else {
+			System.out.println(name + " ( " + description + " ) : " + price);
+		}
+	}
+	
+}
