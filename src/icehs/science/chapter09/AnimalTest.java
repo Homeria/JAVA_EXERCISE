9d57867dbac7884b9a21311f23feb5c5fedbb26d
admin <admin@10.137.138.181>
2018-01-12 13:34:37 +0900

    Chapter09

---
+++ src/icehs/science/chapter09/AnimalTest.java
@@ -0,0 +1,20 @@
+package icehs.science.chapter09;
+
+public class AnimalTest{
+
+	public static void main(String[] args) {
+		
+		Cat c1 = new Cat(3, "其福矫救", "具克捞");
+		Dog d1 = new Dog(2, "矫海府救倾胶虐", "港港捞");
+		
+		c1.hunt();
+		c1.play();
+		c1.run();
+		d1.run();
+		d1.hunt();
+		
+	}
+	
+
+	
+}
